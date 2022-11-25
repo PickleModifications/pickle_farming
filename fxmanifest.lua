36183acec34fe1fffbce33b36319ffd41f246748
Pickle <111543470+PickleModifications@users.noreply.github.com>
2022-11-25 15:19:16 -0500

    Fixed indent

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,19 +8,19 @@ shared_scripts {
 	"config.lua",
 	"bridge/**/shared.lua",
 	"modules/**/shared.lua",
-    "core/shared.lua"
+	"core/shared.lua"
 }
 
 client_scripts {
 	"bridge/**/client.lua",
 	"modules/**/client.lua",
-    "core/client.lua"
+	"core/client.lua"
 }
 
 server_scripts {
 	"bridge/**/server.lua",
 	"modules/**/server.lua",
-    "core/server.lua"
+	"core/server.lua"
 }
 
 lua54 'yes'
