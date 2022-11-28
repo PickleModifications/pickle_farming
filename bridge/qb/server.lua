1b611f68792ea0d7ac873e09c0b25267fbf10cab
Pickle <111543470+PickleModifications@users.noreply.github.com>
2022-11-27 22:32:11 -0500

    QBCore resource name fix

--- bridge/qb/server.lua
+++ bridge/qb/server.lua
@@ -1,6 +1,6 @@
-if GetResourceState('qb_core') ~= 'started' then return end
+if GetResourceState('qb-core') ~= 'started' then return end
 
-QBCore = exports.qb_core:GetCoreObject()
+QBCore = exports["qb-core"]:GetCoreObject()
 
 function RegisterCallback(name, cb)
     QBCore.Functions.CreateCallback(name, cb)
