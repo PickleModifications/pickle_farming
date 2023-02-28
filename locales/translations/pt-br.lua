deec1ca79c5f8c992d81906eb48784cdb9d3acfb
Vinicius Rosa <viniciusrosa3525@gmail.com>
2023-02-28 14:49:52 -0300

    Add pt-br translation

---
+++ locales/translations/pt-br.lua
@@ -0,0 +1,17 @@
+Language["pt-br"] = {
+  success_process_item = "O item foi processado.",
+  success_exchanged_item = "Os itens foram trocados.",
+  doesnt_have_enough_item_to_process = "Você não tem itens para processar.",
+  doesnt_have_any_items_exchange = "Você não tem itens para trocar.",
+  failed_process_item = "Falha ao processar.",
+  process_your_plants = "Pressione ~INPUT_CONTEXT~ para processar suas plantas.",
+  exchange_your_items = "Pressione ~INPUT_CONTEXT~ para trocar seus itens.",
+  doesnt_have_this_seed = "Você não tem nenhuma semente.",
+  too_many_plants_active = "Você tem várias plantas ativas",
+  need_to_have_gardening_pitcher = "Você precisa ter um jarro.",
+  need_to_have_gardening_shovel = "Você precisa ter uma pá.",
+  water_the_plant = "Pressione ~INPUT_CONTEXT~ para regar a planta",
+  harvest_the_plant = "Pressione ~INPUT_CONTEXT~ para pegar a planta.",
+  planted_seed = "Semente plantada.",
+  cant_plant_here = "Você não pode plantar essa semente aqui."
+}
