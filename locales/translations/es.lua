98be0fd6ef97ad21306b200c0d0f60358a91161c
Iggy-dev <71981882+Iggy-dev@users.noreply.github.com>
2023-04-15 04:24:42 +0200

    Add files via upload

---
+++ locales/translations/es.lua
@@ -0,0 +1,17 @@
+Language["es"] = {
+    success_process_item = "Objeto procesado con éxito.",
+    success_exchanged_item = "Artículo(s) intercambiado(s) con éxito.",
+    doesnt_have_enough_item_to_process = "No tienes ningún objeto para procesar.",
+    doesnt_have_any_items_exchange = "No tienes ningún artículo para cambiar.",
+    failed_process_item = "No se pudo procesar el elemento.",
+    process_your_plants = "Pulsa ~INPUT_CONTEXT~ para procesar tus plantas.",
+    exchange_your_items = "Pulsa ~INPUT_CONTEXT~ para cambiar tus artículos.",
+    doesnt_have_this_seed = "No tienes esta semilla.",
+    too_many_plants_active = "Tienes demasiadas plantas activas",
+    need_to_have_gardening_pitcher = "Necesitas tener una jarra de jardinería..",
+    need_to_have_gardening_shovel = "Necesitas tener una pala de jardinería..",
+    water_the_plant = "Pulsa ~INPUT_CONTEXT~ para regar plantas",
+    harvest_the_plant = "Pulsa ~INPUT_CONTEXT~ para cosechar la planta.",
+    planted_seed = "Semillas plantadas",
+    cant_plant_here = "No puedes plantar esta semilla aquí."
+}
