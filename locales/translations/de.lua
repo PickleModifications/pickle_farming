fb36deb857828c800bf709ed254d6859b40a7933
Jey <63811758+JeyCraft@users.noreply.github.com>
2023-04-27 18:30:47 +0200

    de.lua
    Added German Translations to farming. (:

---
+++ locales/translations/de.lua
@@ -0,0 +1,17 @@
+Language["de"] = {
+    success_process_item = "Gegenstand erfolgreich verarbeitet.",
+    success_exchanged_item = "Erfolgreich verkaufte Item(s).",
+    doesnt_have_enough_item_to_process = "Du hast keine oder zuwenige Gegenstände zum verarbeiten.",
+    doesnt_have_any_items_exchange = "Du hast keine Gegenstände zum verkaufen.",
+    failed_process_item = "Gegenstand verarbeiten fehlgeschlagen.",
+    process_your_plants = "Drücke ~INPUT_CONTEXT~ um deine Ernte zu verarbeiten.",
+    exchange_your_items = "Drücke ~INPUT_CONTEXT~ um deine Gegenstände zu verkaufen.",
+    doesnt_have_this_seed = "Du besitzt dieses Saatgut nicht.",
+    too_many_plants_active = "Du hast bereits zuviele Pflanzen im Boden.",
+    need_to_have_gardening_pitcher = "Du benötigst eine Gießkanne.",
+    need_to_have_gardening_shovel = "Du benötigst eine Gärtnerschaufel.",
+    water_the_plant = "Drücke ~INPUT_CONTEXT~ um die Pflanze zu gießen.",
+    harvest_the_plant = "Drücke ~INPUT_CONTEXT~ um die Pflanze zu ernten.",
+    planted_seed = "Saatgut eingepflanzt.",
+    cant_plant_here = "Du kannst dieses Saatgut hier nicht pflanzen."
+}
