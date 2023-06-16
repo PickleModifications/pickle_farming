9219d77c5db163446cc532bc3ef99d9195114813
KNA7022 <89156599+KNA7022@users.noreply.github.com>
2023-06-16 14:42:52 +0800

    Add files via upload

---
+++ locales/translations/zh-cn.lua
@@ -0,0 +1,17 @@
+Language["zh-cn"] = {
+    success_process_item = "成功处理物品.",
+    success_exchanged_item = "成功交换物品.",
+    doesnt_have_enough_item_to_process = "您没有任何要处理的物品.",
+    doesnt_have_any_items_exchange = "你没有物品需要交换.",
+    failed_process_item = "无法处理物品.",
+    process_your_plants = "按下 ~INPUT_CONTEXT~ 处理种植物.",
+    exchange_your_items = "按下 ~INPUT_CONTEXT~ 交换你的物品.",
+    doesnt_have_this_seed = "你没有这个种子.",
+    too_many_plants_active = "你的植物太多了",
+    need_to_have_gardening_pitcher = "你需要一个园艺水罐.",
+    need_to_have_gardening_shovel = "你需要一把园艺铲.",
+    water_the_plant = "按下 ~INPUT_CONTEXT~ 给植物浇水",
+    harvest_the_plant = "按下 ~INPUT_CONTEXT~ 收割植物.",
+    planted_seed = "种植的种子.",
+    cant_plant_here = "你不能在这里种这种种子."
+}
